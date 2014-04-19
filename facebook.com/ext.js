166416453b9839a801926c48508b72bb9cb429fe
Peter Schop <peter@schop.nu>
2014-04-19 17:07:51 +0200

    Facebook test

---
+++ facebook.com/ext.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#pageLogo a').removeAttr('href').mouseover(function(){
+        $('#pageHead').stop().animate('100', {
+            width: 421px,
+            opacity:1.00
+        });
+    }).mouseleave(function(){
+        $('#pageHead').stop.animate('100', {
+            width: 22px,
+            opacity:0.10
+        });
+    });
+});
+
+
