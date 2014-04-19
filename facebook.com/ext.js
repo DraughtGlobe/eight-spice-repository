e60e5fa8259abdb6a3928d4c51d45f99d17aa66c
Peter Schop <peter@schop.nu>
2014-04-19 17:52:33 +0200

    checking

--- facebook.com/ext.js
+++ facebook.com/ext.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
-    $('#pageLogo a').removeAttr('href').mouseover(function(){
-        $('#pageHead').stop().animate('100', {
-            width:'421px',
-            opacity:'1.00'
-        });
-    }).mouseleave(function(){
-        $('#pageHead').stop.animate('100', {
-            width: '22px',
-            opacity:'0.10'
+(function() {
+    $(document).ready(function() {
+        $('#pageLogo a').removeAttr('href').mouseover(function(){
+            $('#pageHead').stop().animate('100', {
+                width:'421px',
+                opacity:'1.00'
+            });
+        }).mouseleave(function(){
+            $('#pageHead').stop.animate('100', {
+                width: '22px',
+                opacity:'0.10'
+            });
         });
     });
-});
+})();
 
 
