ed34d87d3f8c6c106a12463698e1a0c2cb80bb49
Peter Schop <peter@schop.nu>
2014-04-19 17:49:44 +0200

    fixxes

--- facebook.com/ext.js
+++ facebook.com/ext.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $('#pageLogo a').removeAttr('href').mouseover(function(){
         $('#pageHead').stop().animate('100', {
-            width: 421px,
-            opacity:1.00
+            width:'421px',
+            opacity:'1.00'
         });
     }).mouseleave(function(){
         $('#pageHead').stop.animate('100', {
-            width: 22px,
-            opacity:0.10
+            width: '22px',
+            opacity:'0.10'
         });
     });
 });
